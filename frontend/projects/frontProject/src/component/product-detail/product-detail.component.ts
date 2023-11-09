f21deca215c2b3f058506a9e964bba6f250be0ff
Timothy85023117 <s94kiwi9191@gmail.com>
2023-11-09 17:20:49 +0800

    新增Cypher功能

--- frontend/projects/frontProject/src/component/product-detail/product-detail.component.ts
+++ frontend/projects/frontProject/src/component/product-detail/product-detail.component.ts
@@ -1,18 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 import {NgFor} from '@angular/common';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import NeoVis, { NeovisConfig, NonFlatNeovisConfig } from "neovis.js";
+import NeoVis, { NeovisConfig } from "neovis.js";
 import {MatInputModule} from '@angular/material/input';
-import { AbstractControl, FormControl, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSliderModule} from '@angular/material/slider';
 import { MAT_COLOR_FORMATS, NgxMatColorPickerModule, NGX_MAT_COLOR_FORMATS } from '@angular-material-components/color-picker';
 import { ThemePalette } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import * as neo4j from 'neo4j-driver';
+
 export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
   symbol: string;
+}
+
+interface Food {
+  value: string;
+  viewValue: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -36,13 +46,17 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./product-detail.component.css'],
   templateUrl: './product-detail.component.html',
   standalone: true,
-  imports: [MatButtonModule, MatInputModule, MatTableModule, MatIconModule, NgFor, FormsModule, NgxMatColorPickerModule],
+  imports: [MatButtonModule, MatInputModule, MatTableModule, MatIconModule, NgFor, FormsModule, NgxMatColorPickerModule, MatSelectModule, MatFormFieldModule, MatSliderModule],
   providers: [
     { provide: MAT_COLOR_FORMATS, useValue: NGX_MAT_COLOR_FORMATS }
    ],
 })
+export class ProductDetailComponent implements AfterViewInit{
 
-export class ProductDetailComponent {
+  ngAfterViewInit() {
+    this.getNodeNames();
+  };
+
   hide = true;
   picker: string = '';
   listColors = ['primary', 'accent', 'warn'];
@@ -57,6 +71,74 @@ export class ProductDetailComponent {
   displayedColumns: string[] = ['name', 'weight', 'symbol', 'position'];
   columnsToDisplay: string[] = this.displayedColumns.slice();
   data: PeriodicElement[] = ELEMENT_DATA;
+
+ driver = neo4j.driver('bolt://10.20.30.34:7687', neo4j.auth.basic('neo4j', 'password'));
+ session = this.driver.session();
+
+//  模擬API回傳值
+ async getNodeNames() {
+  try {
+    const result = await this.session.run('MATCH (n) RETURN distinct labels(n), count(*)');
+    const nodeNames = result.records.map((record: any) => record._fields[0][0]);
+    console.log(nodeNames);
+  } catch (error) {
+    console.error('Error executing Neo4j query:', error);
+  } finally {
+    await this.session.close();
+    await this.driver.close();
+  }
+}
+
+//
+
+
+  barColor: ThemePalette = 'primary';
+  lengthValue = 500;
+  maxValue = 1000;
+
+  limitCount: number = 10;
+  nodeSize: number = 100;
+  selectedValue: string = "All";
+  selectedSubValue: string = "All";
+  selectedTypeValue: string = "All";
+  foods: Food[] = [
+    {value: 'All', viewValue: '全部'},
+    {value: 'Person', viewValue: '演員'},
+  ];
+
+  subCategory: Food[] = [
+    {value: 'All', viewValue: '全部'},
+    {value: 'Movie', viewValue: '電影'},
+  ];
+
+  typeList: Food[] = [
+    {value: 'All', viewValue: '全部'},
+    {value: 'DIRECTED', viewValue: '指導'},
+    {value: 'WROTE', viewValue: '編寫'},
+    {value: 'ACTED_IN', viewValue: '演出'},
+  ];
+
+  selectedNodeData:string[] = [];
+
+  get computedLimitCount(): string {
+    // 在这里计算属性的值，例如，将 originalValue 加倍
+    return this.limitCount !== 0 ? ` LIMIT ${this.limitCount}`: '';
+  }
+
+  get computedValue(): string {
+    // 在这里计算属性的值，例如，将 originalValue 加倍
+    return this.selectedValue !== 'All' ? `:${this.selectedValue}`: '';
+  }
+
+  get computedSubValue(): string {
+    // 在这里计算属性的值，例如，将 originalValue 加倍
+    return this.selectedSubValue !== 'All' ? `:${this.selectedSubValue}`: '';
+  }
+
+  get computedTypeValue(): string {
+    // 在这里计算属性的值，例如，将 originalValue 加倍
+    return this.selectedTypeValue !== 'All' ? `:${this.selectedTypeValue}`: '';
+  }
 
   valueChanged(newValue: string) {
     this.cypherText = newValue;
@@ -87,7 +169,6 @@ export class ProductDetailComponent {
   }
 
   draw() {
-console.log(this.colorCtr.hex)
     var config : NeovisConfig = {
       containerId: "viz",
       neo4j: {
@@ -99,74 +180,168 @@ console.log(this.colorCtr.hex)
           DB: {
               label: "name",
               value: "pagerank",
-              group: "community",
+              size: "20",
               font: {
-                size: '15',
+                size: '12',
                 color: `#${this.mainColor.hex}`
-              }
+              },
           },
           Tool: {
             label: "name",
             value: "pagerank",
-            group: "community",
-            font: {
-              size: '12',
-              color: `#${this.mainColor.hex}`
-            }
-          },
-          Person: {
-            label: "name",
-            value: "pagerank",
-            group: "community",
+            size: "20",
             font: {
               size: '12',
               color: `#${this.mainColor.hex}`
             }
           },
           Movie: {
+            label: "title",
+            value: "pagerank",
+            size: "20",
+            font: {
+              size: '12',
+              color: `#${this.mainColor.hex}`
+            },
+            // color: {
+            //   background: "#e9a89b",
+            //   highlight: "#e9a89b",
+            // }
+          },
+          Person: {
             label: "name",
             value: "pagerank",
-            group: "community",
+            size: "20",
             font: {
               size: '12',
               color: `#${this.mainColor.hex}`
             }
-          }
+          },
       },
       relationships: {
         ACTED_IN: {
           [NeoVis.NEOVIS_ADVANCED_CONFIG]: {
               static: {
                   label: "ACTED_IN",
-                  color: `#${this.lineColor.hex}`,
+                  color: `#${this.lineColor.hex}` || '#000000',
                   font: {
                       "background": "none",
-                      "strokeWidth": "0",
+                      "strokeWidth": "10",
                       "size": 12,
-                      "color": `#${this.colorCtr.hex}`
+                      "color": `#${this.colorCtr.hex}` || '#000000',
                   }
               }
           }
       },
+      WROTE: {
+        [NeoVis.NEOVIS_ADVANCED_CONFIG]: {
+            static: {
+                label: "WROTE",
+                color: `#${this.lineColor.hex}` || '#000000',
+                font: {
+                    "background": "none",
+                    "strokeWidth": "10",
+                    "size": 12,
+                    "color": `#${this.colorCtr.hex}` || '#000000',
+                }
+            }
+        }
+    },
+    DIRECTED: {
+      [NeoVis.NEOVIS_ADVANCED_CONFIG]: {
+          static: {
+              label: "DIRECTED",
+              color: `#${this.lineColor.hex}` || '#000000',
+              font: {
+                  "background": "none",
+                  "strokeWidth": "10",
+                  "size": 12,
+                  "color": `#${this.colorCtr.hex}` || '#000000',
+              }
+          }
+      }
+  },
       CONTAINS: {
           [NeoVis.NEOVIS_ADVANCED_CONFIG]: {
               static: {
                   label: "CONTAINS",
-                  color: `#${this.lineColor.hex}`,
+                  color: `#${this.lineColor.hex}` || '#000000',
                   font: {
                       "background": "none",
-                      "strokeWidth": "0",
+                      "strokeWidth": "10",
                       "size": 12,
-                      "color": `#${this.colorCtr.hex}`
+                      "color": `#${this.colorCtr.hex}` || '#000000',
                   }
               }
           }
       }
       },
-
-      initialCypher: this.cypherText || "MATCH (n1)-[r]->(n2) RETURN r, n1, n2 LIMIT 25"
+      visConfig: {
+        autoResize: true,
+        // clickToUse: true, // 點擊canvas後才可異動
+        nodes: {
+            shape: 'circle',
+            borderWidth: 0,
+            widthConstraint: {
+              minimum: 200,
+              maximum: 200
+          },
+            // physics: false, // 節點可移動到固定位置
+        },
+        physics: {
+          enabled: true,
+          barnesHut: {
+            // centralGravity: 0,
+            // gravitationalConstant: -20000,  //调整引力常数，根据需要适应距离
+            // springLength: 150, // 调整弹簧长度，根据需要适应距离
+          },
+        },
+        edges: {
+            // arrows: {
+            //     to: true,
+            // },
+            color: `#${this.colorCtr.hex}` || '#000000', //連結線顏色
+            length: this.lengthValue, //連結線長度
+            hoverWidth: 200,
+            // smooth: {
+            //   enabled: false,
+            //   type: "WROTE",
+            //   roundness: 0,
+            // },
+        },
+    },
+      initialCypher: this.cypherText ? this.cypherText : `MATCH (n1${this.computedValue})-[r${this.computedTypeValue}]->(n2${this.computedSubValue}) RETURN r, n1, n2${this.computedLimitCount}`
  }
-    var viz = new NeoVis(config);
+    var viz = new NeoVis(config) as any;
     viz.render();
+    var setSelectedNodeData=(data: any)=>{
+      this.selectedNodeData = data;
+    }
+
+    this.selectedNodeData = viz.registerOnEvent('clickNode', function (event: any) {
+      console.log(event.node.raw.properties);
+      const dataList = Object.keys(event.node.raw.properties).map(item=> ` ${item}:${event.node.raw.properties[item]}`)
+      setSelectedNodeData(dataList);
+    });
+
 }
+
+saveCanvasAsImage() {
+  const canvas = document.getElementsByTagName('canvas')[0] as any;
+  canvas.addEventListener('contextmenu', function (e:any) {
+    e.preventDefault(); // 阻止默认的右键上下文菜单弹出
+  });
+  const context = canvas.getContext('2d');
+  context.globalAlpha = 0.5; // 0.5 表示半透明
+  context.font = '90px Arial';
+  context.fillStyle = '#818181'; // 文本颜色
+  context.fillText('浮水印', 300, 280); // 替换为你的文本内容和坐标 (x, y)
+
+  const link = document.createElement('a');
+  link.href = canvas.toDataURL('image/png');
+  link.download = 'neoVisChart.png';
+  link.click();
+  context.globalAlpha = 1; // 恢复为完全不透明
+}
+
 }
